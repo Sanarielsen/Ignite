b4dd19fc430d7e157bb941a01992a26a754e94a9
Samuel Hcp <samuelcarneiro223@gmail.com>
2022-08-07 18:59:15 -0300

    Add sidebar and style in this component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import {Post} from './Post'
 import {Header} from './components/Header'
-import './styles.css'
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {  
   return (
     <div> 
 
       <Header />
-      
-      <h1> Hello world </h1> 
-      <Post 
-        author="Samuel Henrique"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
-      />
 
-      <Post 
-        author="Gui"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
-      />
+      <div className={styles.wrapper}> 
 
-      <Post 
-        author="Gabriel Narimatsu"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
-      />
-    </div> 
+        <Sidebar />
+        
+        <main>
+          <Post 
+            author="Samuel Henrique"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
+          />
+          <Post 
+            author="Gui"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
+          />
+          <Post 
+            author="Gabriel Narimatsu"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo architecto blanditiis quod neque autem, aliquam odit sint nam corporis, voluptatem at ea tempora? Sint aspernatur a vero deserunt aliquid nam." 
+          />    
+        </main>
+      </div>                  
+    </div>
   )
 }
 
